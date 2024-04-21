1cb6687379ca2e993442a8465624e9ccaa6ff405
dhruv <dhruvpatel9755@gmail.com>
2024-04-21 18:48:30 +0530

    loader added

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-const container = document.querySelector(".container");
-const input = document.querySelector(".search_input");
-const searchBtn = document.querySelector(".search_btn");
-let pagination = false;
-let imageData = [];
-let pageNumber = 1;
-let searchQuery = "toys";
-fetchData();
-async function fetchData() {
-  try {
-    const response = await fetch(
-      `https://api.unsplash.com/search/photos?page=${pageNumber}&query=${searchQuery}&client_id=vcvJ9rWOI4mI4QJQ_kMAaD6hOccNZ720NSyf8SGqfas`
-    );
-    const data = await response.json();
-    imageData = data.results;
-    console.log(imageData);
-    renderImages();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-console.log(imageData);
-// showing data
-function renderImages() {
-  if (pagination == false) {
-    container.innerHTML = "";
+window.addEventListener("DOMContentLoaded", () => {
+  let loader = document.querySelector(".box");
+  loader.style.display = "block";
+  const container = document.querySelector(".container");
+  const input = document.querySelector(".search_input");
+  const searchBtn = document.querySelector(".search_btn");
+  let pagination = false;
+  let imageData = [];
+  let pageNumber = 1;
+  let searchQuery = "toys";
+  fetchData();
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos?page=${pageNumber}&query=${searchQuery}&client_id=vcvJ9rWOI4mI4QJQ_kMAaD6hOccNZ720NSyf8SGqfas`
+      );
+      const data = await response.json();
+      imageData = data.results;
+      console.log(imageData);
+      loader.style.display = "none";
+      renderImages();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  imageData.forEach((image) => {
-    const card = document.createElement("card");
-    card.classList.add("card", "custom_card");
-    card.innerHTML = `
+  console.log(imageData);
+  // showing data
+  function renderImages() {
+    if (pagination == false) {
+      container.innerHTML = "";
+    }
+
+    imageData.forEach((image) => {
+      const card = document.createElement("card");
+      card.classList.add("card", "custom_card");
+      card.innerHTML = `
            
           <img class="card-img-top image" src=${
             image.urls.regular
@@ -44,35 +48,40 @@ function renderImages() {
           </div>
         
             `;
-    container.appendChild(card);
-  });
-}
-
-// Pagination
-// const pageBtn = document.querySelector(".page");
-// pageBtn.addEventListener("click", () => {
-//   pagination = true;
-//   pageNumber++;
-//   fetchData();
-// });
-
-// Pagination Function
-function isBottomOfPage() {
-  return window.innerHeight + window.scrollY >= document.body.offsetHeight - 1;
-}
-
-window.addEventListener("scroll", () => {
-  if (isBottomOfPage()) {
-    pagination = true;
-    pageNumber++;
-    fetchData();
+      container.appendChild(card);
+    });
   }
-});
 
-//Search Functionality
-searchBtn.addEventListener("click", () => {
-  pagination = false;
-  pageNumber = 1;
-  searchQuery = input.value;
-  fetchData();
+  // Pagination
+  // const pageBtn = document.querySelector(".page");
+  // pageBtn.addEventListener("click", () => {
+  //   pagination = true;
+  //   pageNumber++;
+  //   fetchData();
+  // });
+
+  // Pagination Function
+  function isBottomOfPage() {
+    return (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 1
+    );
+  }
+
+  window.addEventListener("scroll", () => {
+    if (isBottomOfPage()) {
+      pagination = true;
+      pageNumber++;
+      fetchData();
+    }
+  });
+
+  //Search Functionality
+  searchBtn.addEventListener("click", () => {
+    loader.style.display = "block";
+    pagination = false;
+    pageNumber = 1;
+    searchQuery = input.value;
+    input.value = "";
+    fetchData();
+  });
 });
